266de5a7230da8c46052b29f2b30fc7dfd6bff2f
Jenan Mannette <jenanmannette@gmail.com>
2017-06-08 15:55:49 -0600

    Logic working, need to add play again button

--- TicTacToe/app/src/main/java/com/jenanmannette/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/jenanmannette/tictactoe/MainActivity.java
@@ -2,12 +2,68 @@ package com.jenanmannette.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    // 0 = yellow, 1 = red
+    int activePlayer = 0;
+
+    // 2 means unplayed
+    int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
+    int[][] winningPositions = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void dropIn(View view) {
+        ImageView counter = (ImageView) view;
+        int tappedCounter= Integer.parseInt(counter.getTag().toString());
+
+        if (gameState[tappedCounter] == 2) {
+            gameState[tappedCounter] = activePlayer;
+            counter.setTranslationY(-1000f);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+
+            counter.animate().translationYBy(1000f).rotation(360).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+
+                    String winner = "Red";
+
+                    if (gameState[winningPosition[0]] == 0) {
+                        winner = "Yellow";
+                    }
+
+                    GridLayout grid = (GridLayout) findViewById(R.id.grid);
+                    grid.animate().alpha(.50f).setDuration(800);
+
+                    TextView winnerMessage = (TextView) findViewById(R.id.winnerMessage);
+                    winnerMessage.setText(winner + " is the winner!");
+
+
+                    LinearLayout winnerLayout = (LinearLayout) findViewById(R.id.playAgainLayout);
+                    winnerLayout.setVisibility(View.VISIBLE);
+                }
+            }
+        }
     }
 }
